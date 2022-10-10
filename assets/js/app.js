cfb11bec96faa0e5f91a393d3736d02c840ec9e7
jhorman9 <79775604+jhorman9@users.noreply.github.com>
2022-10-10 02:12:56 -0500

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -14,3 +14,17 @@ open.addEventListener("click", () =>{
 closed.addEventListener("click", () =>{
     ul.style.display ="none";
 })
+
+buttonRight.addEventListener("click", () =>{
+    iconsExperience.innerHTML += `<div class="icon">
+                                    <img src="assets/imgs/Google.png" alt="google">
+                                    <p class="center padding withoutMargin">Frontend and UI Dev</p>
+                                    <h4 class="withoutMargin">6 meses</h4>
+                                </div>
+                                <div class="icon">
+                                    <img src="assets/imgs/Wizeline.png" alt="Wizeline">
+                                    <p class="center padding withoutMargin">Interactive Design</p>
+                                    <h4 class="withoutMargin">1 año</h4>
+                                </div>
+                                <div class="buttonLeft"><</div>
+    `})
