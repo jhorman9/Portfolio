3ea53c97412a4f554545f876528706e2c49596c8
jhorman9 <jhosmita9@gmail.com>
2022-10-10 23:06:44 -0500

    actualizando

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,7 +5,9 @@ const buttonRight = document.querySelector(".button-right");
 const iconsExperience = document.querySelector(".icons--experience")
 
 open.addEventListener("click", () =>{
+
     ul.style.display = "flex";
+
     if(ul.style.display == "flex") {
         closed.style.display ="block";
     }else{
@@ -18,15 +20,17 @@ closed.addEventListener("click", () =>{
 })
 
 buttonRight.addEventListener("click", () =>{
-    iconsExperience.innerHTML += `<div class="icon">
-                                    <img src="assets/imgs/Google.png" alt="google">
-                                    <p class="center padding withoutMargin">Frontend and UI Dev</p>
-                                    <h4 class="withoutMargin">6 meses</h4>
-                                </div>
-                                <div class="icon">
-                                    <img src="assets/imgs/Wizeline.png" alt="Wizeline">
-                                    <p class="center padding withoutMargin">Interactive Design</p>
-                                    <h4 class="withoutMargin">1 año</h4>
-                                </div>
-                                <div class="buttonLeft"><</div>
-    `})
+    iconsExperience.innerHTML += `
+    <div class="icon icon4">
+        <img src="assets/imgs/Google.png" alt="google">
+        <p class="center padding withoutMargin">Frontend and UI Dev</p>
+        <h4 class="withoutMargin">6 meses</h4>
+    </div>
+    <div class="icon icon1">
+        <img src="assets/imgs/Wizeline.png" alt="Wizeline">
+        <p class="center padding withoutMargin">Interactive Design</p>
+        <h4 class="withoutMargin">1 año</h4>
+    </div>
+    `
+});
+
